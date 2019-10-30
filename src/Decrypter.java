fa1b53452c03a414aee9f3a04a3b32dfa1c5b951
LikeARocket <Max.Deutschmann@jambit.com>
2019-10-30 17:01:33 +0100

    decryption works!!!

--- src/Decrypter.java
+++ src/Decrypter.java
@@ -1,13 +1,15 @@
+import java.util.Scanner;
+
 public class Decrypter {
-    public String decrypterrrrr(String messageToBeEncrypted, int offset){
-        String encryptedMessage;
-char[] charArray = messageToBeEncrypted.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            char[] newArray = new char[charArray.length];
-            int deferal = (charArray[i] + offset)%95;
 
+    public void decrypt(String message, int key) {
+        char[] singleCharacters = message.toCharArray();
+        System.out.println("Definiere den Offset");
+        for (int i = 0; i < singleCharacters.length ; i++) {
+            int s = singleCharacters[i];
+            int offset = Constants.CHARACTER_SET.indexOf((singleCharacters[i])) + key;
+            System.out.println(offset);
+            System.out.println(Constants.CHARACTER_SET.charAt(offset));
         }
-        return encryptedMessage;
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        UserInput userInput = new UserInput();
-        userInput.input();
-        Scanner sc = new Scanner(System.in);
+        //UserInput userInput = new UserInput();
+        //userInput.input();
+        //Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        int key = sc.nextInt();
+
+        Decrypter decrypter = new Decrypter();
+        decrypter.decrypt(message, key);
 
     }
 }
